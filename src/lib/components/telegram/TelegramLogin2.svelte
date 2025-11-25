86175fd768fe0a4686db6f90dadb2d504c3c1c84
saha <acidoralkali@gmail.com>
2025-11-26 00:39:02 +0200

    test telegram auth

--- src/lib/components/telegram/TelegramLogin2.svelte
+++ src/lib/components/telegram/TelegramLogin2.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-	function handlerLogin() {
-		console.log('кнопка');
-		//8367276457:AAHme_R21pjgAxz3g0Sk4DqQWiSfSJGEH1M
-		window.Telegram.Login.auth({ bot_id: '8367276457', request_access: true }, (data) => {
-			if (!data) {
-				// failed
-			}
-			console.log('data', data);
-		});
-	}
+	// function handlerLogin() {
+	// 	window.Telegram.Login.auth({ bot_id: '8367276457', request_access: true }, (data) => {
+	// 		if (!data) {
+	// 			// failed
+	// 		}
+	// 		console.log('data', data);
+	// 	});
+	// }
 </script>
 
-<button onclick={handlerLogin}>Login via Telegram</button>
+<!-- <button onclick={handlerLogin}>Login via Telegram</button> -->
+<a href="https://t.me/kyivgoldbot?start">Login via Telegram</a>
