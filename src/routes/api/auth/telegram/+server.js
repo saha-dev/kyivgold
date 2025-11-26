2ad7b7bfa11ee351dc10cad1912fa987679c78f9
saha <acidoralkali@gmail.com>
2025-11-26 20:46:57 +0200

    test telegram auth

--- src/routes/api/auth/telegram/+server.js
+++ src/routes/api/auth/telegram/+server.js
@@ -3,4 +3,5 @@ export async function GET({ url, cookies }) {
 	// 1. Отримання даних із параметрів URL
 	const authData = Object.fromEntries(url.searchParams);
 	console.log(authData);
+	return new Response('OK');
 }
