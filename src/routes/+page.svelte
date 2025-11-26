1832901ca299fe4ea18c552f462991f824d8bc68
saha <acidoralkali@gmail.com>
2025-11-26 19:07:46 +0200

    test telegram auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
-	// Ця функція викликається Telegram після успішної авторизації
-	function onTelegramAuth(user: any) {
-		console.log('Авторизація успішна:', user);
-	}
-
-	onMount(() => {
-		window.onTelegramAuth = onTelegramAuth;
-	});
 </script>
 
 <div class="telegram-login-container">
@@ -18,7 +8,7 @@
 		src="https://telegram.org/js/telegram-widget.js?22"
 		data-telegram-login="kyivgoldbot"
 		data-size="large"
-		data-onauth="onTelegramAuth(user)"
+		data-auth-url="https://kyivgold.pp.ua/workshop/hs/telegram/auth"
 		data-request-access="write"
 	></script>
 </div>
