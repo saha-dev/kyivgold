4b07ee7d3cb8867038e6f1c8a95a411a48b0bdd1
saha <acidoralkali@gmail.com>
2025-11-27 22:58:36 +0200

    test telegram auth

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,8 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ request }) => {
 	const ua = request.headers.get('user-agent') || '';
-	const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+
+	const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile/i.test(ua);
 
 	return { isMobile, ua };
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,8 @@
 	import TelegramLogin from '$lib/components/telegram/TelegramLogin.svelte';
 	const { data } = $props();
 	const { isMobile, ua } = data;
-
-	console.log('ua', ua);
 </script>
 
-<h3>{isMobile ? 'Mobile' : 'PC'}</h3>
+<h3>{!isMobile ? 'PC' : 'Mobile'}</h3>
 <h4>{ua}</h4>
 <TelegramLogin />
